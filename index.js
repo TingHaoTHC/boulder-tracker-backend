55fae79773f40a8b0c24241f0c5611ba483eed63
Ting Hao Chang <x16370076@student.ncirl.ie>
2026-07-31 18:42:54 +0100

    Initial backend setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000; 
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Boulder Tracker Backend is Live!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
